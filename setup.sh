2afad8b15d5e4dff1852145f011ae11e5669cb11
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-05-09 13:25:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,33 +2,38 @@
 
 # Create folders
 cd ~
-mkdir .dotfiles Allendevaux Exploits CustomCodes Post-Exploitation Pre-Exploitation Offensive-Security Github-Repos Wordlist
+mkdir .dotfiles Allendevaux Exploits CustomScripts Post-Exploitation Platform Offensive-Security Github-Repos Wordlist Temp Test
 cd ~/Post-Exploitation
 mkdir Linux Windows
-cd ~/Pre-Exploitation
-mkdir Reverse-Shell
+cd ~/Platform
+mkdir Linux Windows WebApp
+cd WebApp
+mkdir OWASP
 
+# Download .dotfile contents from github
+cd ~
 cd .dotfiles
-wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/directory-fuzzer"
-wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/misc"
-wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/scanning-aliases"
-wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/system-aliases"
-wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/to-be-sorted"
+# wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/directory-fuzzer"
+# wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/misc"
+# wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/scanning-aliases"
+# wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/system-aliases"
+# wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/to-be-sorted"
+wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/.dotfiles(aliases)/allcustomcommands"
 
 cd ~
-cd CustomCodes
+cd CustomScripts
 wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/CustomCodes/url-decode.py"
+wget "https://raw.githubusercontent.com/CipherStriker/System-Backup/main/CustomCodes/url-encode.py"
 
 # cd ~
 # cd Wordlist
 # git clone https://github.com/danielmiessler/SecLists.git
 
-
-
-# Source the alias files
+# Source all alias files
 cd ~
-echo "source ~/.dotfiles/directory-fuzzer" >> .zshrc
-echo "source ~/.dotfiles/scanning-aliases" >> .zshrc
-echo "source ~/.dotfiles/system-aliases" >> .zshrc
-echo "source ~/.dotfiles/to-be-sorted" >> .zshrc
-echo "source ~/.dotfiles/misc" >> .zshrc
+# echo "source ~/.dotfiles/directory-fuzzer" >> .zshrc
+# echo "source ~/.dotfiles/scanning-aliases" >> .zshrc
+# echo "source ~/.dotfiles/system-aliases" >> .zshrc
+# echo "source ~/.dotfiles/to-be-sorted" >> .zshrc
+# echo "source ~/.dotfiles/misc" >> .zshrc
+echo "source ~/.dotfiles/allcustomcommands" >> .zshrc
