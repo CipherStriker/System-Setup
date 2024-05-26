15a5bcc3e9c704bb7ae23440057d80aefa814488
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-05-26 18:45:09 +0530

    Update setup1.1.sh

--- setup1.1.sh
+++ setup1.1.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 echo "Creating Directories"
-mkdir .dotfiles Allendevaux Exploits Offensive-Security Github-Repos Wordlist Temp Test
-
+mkdir Allendevaux Exploits Offensive-Security Github-Repos Wordlist Temp Test
+echo ""
 
 echo "Downloading System-Backup repository"
+echo ""
 cd
 git clone https://github.com/CipherStriker/System-Backup.git
 
+echo ""
+echo "Transfering files from System-Backup to designated directories"
 mv System-Backup/* $HOME/.
+mv 'System-Backup/.dotfiles(aliases)' $HOME/.dotfiles
 rm -rf System-Backup
-
+echo ""
+echo "Downloading SecLists in Wordlist directory"
 #cd $HOME/Wordlist
 #git clone https://github.com/danielmiessler/SecLists.git
 
+echo ""
+echo "Sourcing files in .zshrc file"
 # Source all alias files
 cd ~
 echo "source $HOME/.dotfiles/allcustomcommands" >> .zshrc
